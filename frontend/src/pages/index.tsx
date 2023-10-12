995018c745c4b5358870203f2235be4dcae2ca18
Marvin Kurland <bugemarvin@outlook.com>
2023-10-12 14:54:03 +0300

    fixing project structure

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+
+export default function Page() {
+  return (
+    <div className={styles.container}>
+        <div className={styles.container_hero}>
+                <span className={styles.container_hero_bg}></span>
+        </div>
+    </div>
+  );
+}
