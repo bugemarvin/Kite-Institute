d6a1a7923bb1d4f8287ef98dbbf6be12e28a1fe4
Marvin Kurland <bugemarvin@outlook.com>
2023-10-14 13:34:28 +0300

    adding site meta stags

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -8,6 +8,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property='og:title' content='Kite Institute of Professional Studies' />
+        <meta property='og:description' content='Kite Institute of Professional Studies is an organization that provides tutoring services to students in Kitengela Area.' />
         <title>Kite Institute of Professional Studies</title>
       </Head>
       <Component {...pageProps} />;
